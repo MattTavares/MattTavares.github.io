41453bdd8e643f67b17513b2b33bf930cc6b7b1f
MattTavares <Matt@TavaresLabs.com>
2015-08-05 16:20:40 -0500

    template
    template

---
+++ README.txt
@@ -0,0 +1,17 @@
+------------------
+Spectrum Support
+------------------
+
+Thank you for choosing Spectrum! If you run into any issues with using this template,
+please don't hesitate to send us a message on our profile at WrapBootstrap.
+https://wrapbootstrap.com/user/StartBootstrap
+
+Since you've purchased this template, you will receive dedicated email support as part of the purchase.
+We will try our best to answer your emails as soon as we can!
+
+------------------
+Spectrum Docs
+------------------
+
+The complete theme documentation is available in the /docs/index.html file.
+Please access that file for complete usage instructions! If you have a question, feel free to email us!
---
+++ mail/contact_me.php
@@ -0,0 +1,24 @@
+<?php
+// check if fields passed are empty
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// create email body and send it	
+$to = 'YOUR-EMAIL@YOUR-WEBSITE.com'; // ****PUT YOUR EMAIL ADDRESS HERE****
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: CONTACT-FORM@YOUR-WEBSITE.com\n"; // ****ENTER WHO YOU WANT THE MESSAGE TO BE FROM HERE****
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
