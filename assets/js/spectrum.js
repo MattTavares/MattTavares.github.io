ca5b14a0cc184acd180853d65071565db5d0555d
MattTavares <Matt@TavaresLabs.com>
2015-08-13 11:46:02 -0500

    potential multi carousel bug fix

--- assets/js/spectrum.js
+++ assets/js/spectrum.js
@@ -82,21 +82,8 @@ jQuery(document).ready(function() {
 
     });
 
-    $("#project-details-carousel").owlCarousel({
-
-        navigation: true,
-        slideSpeed: 300,
-        paginationSpeed: 400,
-        singleItem: true,
-        autoHeight: true,
-        navigationText: [
-            "<i class='fa fa-angle-left'></i> Prev",
-            "Next <i class='fa fa-angle-right'></i>"
-        ],
-
-    });
-
-        $("#project-details-carousel-2").owlCarousel({
+    //Should match all ids starting with #project-details-carousel
+    $("input[id^='project-details-carousel']).owlCarousel({
 
         navigation: true,
         slideSpeed: 300,
