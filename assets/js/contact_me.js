e64649862a7aa839876a1f578c581e9a528ca784
MattTavares <Matt@TavaresLabs.com>
2015-08-18 17:23:16 -0500

    moved contact_me.php to external php enabled server

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -17,7 +17,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://contact.tavareslabs.com/php/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
