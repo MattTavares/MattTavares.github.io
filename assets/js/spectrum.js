378c55fb4d55156e1159c1677f6edcf9684aa393
MattTavares <Matt@TavaresLabs.com>
2015-08-19 10:18:11 -0500

    Fix carousels

--- assets/js/spectrum.js
+++ assets/js/spectrum.js
@@ -78,12 +78,6 @@ jQuery(document).ready(function() {
         autoWidth: true,
         items: 3,
         itemsScaleUp: true,
-        afterUpdate: function () {
-            updateSize();
-        },
-        afterInit:function(){
-            updateSize();
-        }
         //transitionStyle: "backSlide"
         //navigationText: [
         //    "<i class='fa fa-angle-left'></i> Prev",
@@ -118,24 +112,6 @@ jQuery(document).ready(function() {
     }).on("blur", ".floating-label-form-group", function() {
         $(this).removeClass("floating-label-form-group-with-focus");
     });
-
-    function updateSize(){
-        var minHeight=parseInt($('.owl-item').eq(0).css('height'));
-        $('.owl-item').each(function () {
-            var thisHeight = parseInt($(this).css('height'));
-            minHeight=(minHeight<=thisHeight?minHeight:thisHeight);
-        });
-        $('.owl-wrapper-outer').css('height',minHeight+'px');
-
-        /*show the bottom part of the cropped images*/
-        $('.owl-carousel .owl-item img').each(function(){
-            var thisHeight = parseInt($(this).css('height'));
-            if(thisHeight>minHeight){
-                $(this).css('margin-top',-1*(thisHeight-minHeight)+'px');
-            }
-        });
-
-    }
 
 });
 
