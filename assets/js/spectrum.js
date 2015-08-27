2f19245c1c35fb48374dbaf03f522487e9ba33d6
MattTavares <Matt@TavaresLabs.com>
2015-08-27 13:57:36 -0500

    attempt to better handle back button presses when in a modal

--- assets/js/spectrum.js
+++ assets/js/spectrum.js
@@ -116,7 +116,19 @@ jQuery(document).ready(function() {
     });
 
     // Tooltip
-        $('[data-toggle="tooltip"]').tooltip();  
+    $('[data-toggle="tooltip"]').tooltip();  
+
+
+    // Close modals on Back Button press instead of leaving the page
+    $(".modal").on("shown.bs.modal", function()  { // any time a modal is shown
+        var urlReplace = "#" + $(this).attr('id'); // make the hash the id of the modal shown
+        history.pushState(null, null, urlReplace); // push state that hash into the url
+    });
+
+    // If a pushstate has previously happened and the back button is clicked, hide any modals.
+    $(window).on('popstate', function() { 
+        $(".modal").modal('hide');
+    });
 });
 
 // Functions to run on window load
